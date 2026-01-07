8083e1a3014aeec69161b615d4698829cbb29dde
Lucas Assis <lukas.ecomp@gmail.com>
2026-01-07 17:41:06 -0300

    change store to perform claim Token operation and update token list

--- stores/tokens.store.ts
+++ stores/tokens.store.ts
@@ -4,23 +4,45 @@ import * as service from '@/services/tokens.service';
 
 interface TokensState {
   tokens: Token[];
-  totalTokens?: number;
+  totalTokens: number;
   loading: boolean;
   error?: string;
+  status?: string;
+
   fetchTokens: (status?: string) => Promise<void>;
+  claimToken: (userId: string) => Promise<void>;
 }
 
-export const useTokensStore = create<TokensState>((set) => ({
+export const useTokensStore = create<TokensState>((set, get) => ({
   tokens: [],
+  totalTokens: 0,
+  lastClaimedToken: undefined,
   loading: false,
+  error: undefined,
+  status: undefined,
 
-  fetchTokens: async (status) => {
+  fetchTokens: async (status?: string) => {
+    set({ loading: true, error: undefined, status });
+
     try {
-      set({ loading: true, error: undefined });
       const response = await service.listTokens(status);
       set({ tokens: response.items, totalTokens: response.meta.total });
     } catch {
-      set({ error: 'Erro ao carregar tokens' });
+      set({ error: 'Erro ao carregar lista de tokens' });
+    } finally {
+      set({ loading: false });
+    }
+  },
+
+  claimToken: async (userId: string) => {
+    try {
+      set({ loading: true, error: undefined, lastClaimedToken: null });
+      const response = await service.claimToken(userId);
+      const { status, fetchTokens } = get();
+      await fetchTokens(status);
+      set({ lastClaimedToken: response });
+    } catch {
+      set({ error: 'Erro ao realizar requisição de Token' });
     } finally {
       set({ loading: false });
     }
