bf4197a7796f7fa5492fe5ce3f7432e618dceb16
Lucas Assis <lukas.ecomp@gmail.com>
2026-01-07 17:42:11 -0300

    add lastClaimedToken to TokensState

--- stores/tokens.store.ts
+++ stores/tokens.store.ts
@@ -5,6 +5,7 @@ import * as service from '@/services/tokens.service';
 interface TokensState {
   tokens: Token[];
   totalTokens: number;
+  lastClaimedToken: Token | null | undefined;
   loading: boolean;
   error?: string;
   status?: string;
